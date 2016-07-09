eb0b7b6cc7a213874f10cc4a57fbb53302e82032
kjnam <kyujnam@gmail.com>
2016-07-09 11:22:58 +0900

    Edited DaoFactory 적용

--- src/main/java/springbook/user/dao/UserDaoTest.java
+++ src/main/java/springbook/user/dao/UserDaoTest.java
@@ -1,14 +1,16 @@
 package springbook.user.dao;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import springbook.user.domain.User;
 
 import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String [] args) throws ClassNotFoundException, SQLException {
-        ConnectionMaker connectionMaker = new NConnectionMaker();
 
-        UserDao dao = new UserDao(connectionMaker);
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
         user.setId("iamkyu");
