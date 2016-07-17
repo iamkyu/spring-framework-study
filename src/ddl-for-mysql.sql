48688ecea8bccea531715ac9e48007cbf4e29da4
kjnam <kyujnam@gmail.com>
2016-07-17 22:32:32 +0900

    Edit 테스트용DBMS mysql -> h2 교체

--- src/ddl-for-mysql.sql
+++ src/ddl-for-mysql.sql
@@ -1,8 +1,8 @@
-create database testdb;
+-- create database testdb;
+-- use testdb;
 
-use testdb;
-
-create table `testdb`.users (
+-- create table `testdb`.users (
+create table users (
     id varchar(10) primary key,
     name varchar(20) not null,
     password varchar(10) not null
