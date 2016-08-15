1b7d90d2ea116db17919363dcf1d5d23af362c15
kjnam <kyujnam@gmail.com>
2016-08-15 19:42:15 +0900

    test: 포인트컷표현식테스트

---
+++ src/main/java/springbook/learningtest/spring/pointcut/Bean.java
@@ -0,0 +1,11 @@
+package springbook.learningtest.spring.pointcut;
+
+/**
+ * @author Kj Nam
+ * @since 2016-08-15
+ */
+public class Bean {
+    public void method() throws RuntimeException {
+
+    }
+}
