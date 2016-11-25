d1150f62a03108ec91040a16d572253365994b35
kjnam <kyujnam@gmail.com>
2016-11-26 00:44:13 +0900

    set boiler-plate

--- src/main/java/springbook/learningtest/template/LineCallback.java
+++
@@ -1,9 +0,0 @@
-package springbook.learningtest.template;
-
-/**
- * @author Kj Nam
- * @since 2016-07-17
- */
-public interface LineCallback<T> {
-    T doSomethingWithLine(String line, T value);
-}
