6c0f73cd6696f7b192388f35709bde2fd6cd5e24
kjnam <kyujnam@gmail.com>
2016-11-29 15:05:46 +0900

    jdbc 컨텍스트 분리

---
+++ src/main/java/springbook/user/dao/JdbcContext.java
@@ -0,0 +1,43 @@
+package springbook.user.dao;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class JdbcContext {
+    private DataSource dataSource;
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void workWithStatementStrategy(StatementStretegy st) throws SQLException {
+        Connection c = null;
+        PreparedStatement ps = null;
+
+        try {
+            c = this.dataSource.getConnection();
+            ps = st.makeStatemnt(c);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -15,27 +15,31 @@ import java.sql.SQLException;
  * @since 2016-11-26
  */
 public class UserDao {
+    private JdbcContext jdbcContext;
     private DataSource dataSource;
+
+    public void setJdbcContext(JdbcContext jdbcContext) {
+        this.jdbcContext = jdbcContext;
+    }
 
     public void setDataSource(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
-        StatementStretegy st = new StatementStretegy() {
-            @Override
-            public PreparedStatement makeStatemnt(Connection c) throws SQLException {
-                PreparedStatement ps = c.prepareStatement(
-                        "insert into users(id, name, password) values(?, ?, ?)");
-                ps.setString(1, user.getId());
-                ps.setString(2, user.getName());
-                ps.setString(3, user.getPassword());
+        this.jdbcContext.workWithStatementStrategy(
+                new StatementStretegy() {
+                    @Override
+                    public PreparedStatement makeStatemnt(Connection c) throws SQLException {
+                        PreparedStatement ps = c.prepareStatement(
+                                "insert into users(id, name, password) values(?, ?, ?)");
+                        ps.setString(1, user.getId());
+                        ps.setString(2, user.getName());
+                        ps.setString(3, user.getPassword());
 
-                return ps;
-            }
-        };
-
-        jdbcContextWithStatementStrategy(st);
+                        return ps;
+                    }
+                });
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
@@ -66,14 +70,13 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        StatementStretegy st = new StatementStretegy() {
+        this.jdbcContext.workWithStatementStrategy(
+                new StatementStretegy() {
             @Override
             public PreparedStatement makeStatemnt(Connection c) throws SQLException {
                 return c.prepareStatement("delete from users");
             }
-        };
-
-        jdbcContextWithStatementStrategy(st);
+        });
     }
 
     public int getCount() throws SQLException {
@@ -90,34 +93,5 @@ public class UserDao {
         c.close();
 
         return count;
-    }
-
-    public void jdbcContextWithStatementStrategy(StatementStretegy st) throws SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-
-        try {
-            c = dataSource.getConnection();
-            ps = st.makeStatemnt(c);
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            if (c != null) {
-                try {
-                    c.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
     }
 }
