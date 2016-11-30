f0dacb1fdfafff6a655385b91e30e0200e6fbc34
kjnam <kyujnam@gmail.com>
2016-11-30 17:36:07 +0900

    getAll 메소드 추가

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -7,6 +7,7 @@ import springbook.user.domain.User;
 import javax.sql.DataSource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 
 /**
@@ -50,4 +51,18 @@ public class UserDao {
     public int getCount() throws SQLException {
         return this.jdbcTemplate.queryForObject("select count(*) from users", new Object[] {}, Integer.class);
    }
+
+    public List<User> getAll() {
+        return this.jdbcTemplate.query("select * from users order by id",
+                new RowMapper<User>() {
+                    @Override
+                    public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                        User user = new User();
+                        user.setId(rs.getString("id"));
+                        user.setName(rs.getString("name"));
+                        user.setPassword(rs.getString("password"));
+                        return user;
+                    }
+                });
+    }
 }
